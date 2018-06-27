58844cad9ce5a68d3c754f7c1647d72ef5504034
lemonjai <lappangcheung@gmail.com>
2018-06-27 17:07:12 -0400

    setup bootstrap navbar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // Custom components
-import Navbar from './layout/Navbar'
-import Landing from './layout/Landing'
-import Footer from './layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Footer from './components/layout/Footer'
+
+// Import CSS
+import './css/App.css'
 
 class App extends Component {
     render(){
         return (
             <div>
                 <Navbar />
-                <Landing />
                 <Footer />
             </div>
         )
