d3b9dd1a69c644ec75c016aa1b0123cd23dc0501
lemonjai <lappangcheung@gmail.com>
2018-06-27 16:32:47 -0400

    setup the front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
+// Custom components
+import Navbar from './layout/Navbar'
+import Landing from './layout/Landing'
+import Footer from './layout/Footer'
+
 class App extends Component {
     render(){
         return (
             <div>
-                Hello
+                <Navbar />
+                <Landing />
+                <Footer />
             </div>
         )
     }
