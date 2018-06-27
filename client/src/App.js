69b67b0d14994b3e6474fc379215caf76455e70d
lemonjai <lappangcheung@gmail.com>
2018-06-27 17:20:18 -0400

    landing page static done

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
         return (
             <div>
                 <Navbar />
+                <Landing />
                 <Footer />
             </div>
         )
