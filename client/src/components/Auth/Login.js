70f0b848c0b3216097ea9da5b40737521f973a98
lemonjai <lappangcheung@gmail.com>
2018-06-29 02:23:27 -0400

    finding a way to change the isAuth state

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -54,6 +54,7 @@ class Login extends Component {
 							<h1 className="display-4 text-center">Log In</h1>
 							<p className="lead text-center">Sign in to your DevConnector account</p>
 							<form onSubmit={this.onSubmit}>
+								
 								<div className="form-group">
 									{/* Email input */}
 									<input 
